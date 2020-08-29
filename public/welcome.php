bebfaa848d00cf58225fd246d5dea43c6956479b
Dawoon Choi <u3207952@uni.canberra.edu.au>
2020-08-29 22:02:03 +1000

    change
    29/08/20 10:01 Pm

--- public/welcome.php
+++ public/welcome.php
@@ -22,19 +22,20 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){ //Check use
 <body>
     <div class="page-header">
         <h1>Hello, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to Semester2 assignment tracker site!</h1>
+    <br>
     </div>
-    <p>
+    <p style="text-align: right">
         <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
-        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>&ensp;&ensp;
     </p>
-    <br><br>
+    <br>
     <p>
         <div style="border:2px solid Tomato;">
-        <h4><strong>Do you want see assignment due date? Click below! </strong></h4> 
+        <h4><strong>Do you want see assignment due date? Click below! </strong></h4><br>
         <a href="read.php" class="btn btn-success">Show assignment list</a> <br><br></div>
-        <br>
+        <br><br>
         <div style="border:2px solid DodgerBlue;">
-        <h4>Edit Information</h4>
+        <h4><strong>Data Management</strong></h4><br>
         <a href="create.php" class="btn btn-primary">create new</a> &ensp;&ensp;&ensp;&ensp;
         <a href="update.php" class="btn btn-info">update list</a> &ensp;&ensp;&ensp;&ensp;
         <a href="delete.php" class="btn btn btn-danger">Delete list</a> &ensp;&ensp;&ensp;&ensp; <br><br>
